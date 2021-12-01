bdf8ede2a54b4b67a7229bc7bf1c8e90c4a2b667
Will Howes <willmhowes@gmail.com>
2021-12-01 13:08:54 -0800

    formalize code as functions, write default call behavior

--- day1/day01.1.py
+++ day1/day01.1.py
@@ -3,11 +3,14 @@
 # Goal: Count the number of times a depth measurement increases
 #       from the previous measurement.
 
-from puzzleinput01 import ls
+from puzzleinput01 import ls as ex01
 
-i,ct = 1,0
-while i < len(ls):
-    if ls[i] > ls[i-1]: ct+=1
-    i+=1
+def ctPosDelta (ls):
+    i,ct = 1,0
+    while i < len(ls):
+        if ls[i] > ls[i-1]: ct+=1
+        i+=1
+    return ct
 
-print(ct)
+if __name__ == "__main__":
+    print(ctPosDelta(ex01))
--- day1/day01.2.py
+++ day1/day01.2.py
@@ -4,13 +4,16 @@
 #       Count the number of times the sum of measurements in
 #       this sliding window increases from the previous sum
 
-from puzzleinput01 import ls
+from puzzleinput01 import ls as ex01
 
-i,ct = 2,0
-while i < len(ls)-1:
-    # can simplify each window to the unique term
-    # between neighboring windows
-    if ls[i+1] > ls[i-2]: ct+=1
-    i+=1
+def ctWindowIncrease(ls):
+    i,ct = 2,0
+    while i < len(ls)-1:
+        # can simplify each window to the unique term
+        # between neighboring windows
+        if ls[i+1] > ls[i-2]: ct+=1
+        i+=1
+    return ct
 
-print(ct)
+if __name__ == "__main__":
+    print(ctWindowIncrease(ex01))
