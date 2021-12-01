d8294586a2c856d46d264afe3387e06b18e8b5ab
Will Howes <willmhowes@gmail.com>
2021-12-01 11:44:31 -0800

    finish day1

---
+++ day1/increase.py
@@ -0,0 +1,13 @@
+# Day 01.1 - Advent of Code
+# Author: Will Howes
+# Goal: Count the number of times a depth measurement increases
+#       from the previous measurement.
+
+from puzzleinput import ls
+
+i,ct = 1,0
+while i < len(ls):
+    if ls[i] - ls[i-1] > 0: ct+=1
+    i+=1
+
+print(ct)
---
+++ day1/increase3.py
@@ -0,0 +1,16 @@
+# Day 01.2 - Advent of Code
+# Author: Will Howes
+# Goal: Consider sums of a three-measurement sliding window.
+#       Count the number of times the sum of measurements in
+#       this sliding window increases from the previous sum
+
+from puzzleinput import ls
+
+i,ct = 2,0
+while i < len(ls)-1:
+    # can simplify each window to the unique term
+    # between neighboring windows
+    if ls[i+1] - ls[i-2] > 0: ct+=1
+    i+=1
+
+print(ct)
