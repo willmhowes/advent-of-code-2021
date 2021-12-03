67d566d81159f09ce1466f54a3d84adb0f5aa824
Will Howes <willmhowes@gmail.com>
2021-12-03 13:16:38 -0800

    use regex

--- day02/day02.1.py
+++ day02/day02.1.py
@@ -5,17 +5,19 @@
 #       get if you multiply your final horizontal position by
 #       your final depth?
 
+import re
+
 def giveInput():
     with open('puzzleinput02.txt', 'r') as file:
-        return [x[0:-1] for x in file.readlines()]
+        p = re.compile(r'(\w)\w+ (\d+)')
+        return re.findall(p, file.read())
 
 def calcMultiple(ls):
-    h = 0
-    d = 0
+    h,d = 0,0
     for c in ls:
-        if c[0] == 'f': h+=int(c[-1])
-        elif c[0] == 'd': d+=int(c[-1])
-        elif c[0] == 'u': d-=int(c[-1])
+        if c[0] == 'f': h+=int(c[1])
+        elif c[0] == 'd': d+=int(c[1])
+        elif c[0] == 'u': d-=int(c[11])
     return h*d
 
 if __name__ == "__main__":
