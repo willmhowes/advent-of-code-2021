e9c1e78601d89fc2dc3206641ed6db539e1fceda
Will Howes <willmhowes@gmail.com>
2021-12-04 11:52:23 -0800

    use bingo module in day04.1

--- day04/day04.1.py
+++ day04/day04.1.py
@@ -4,59 +4,7 @@
 #       out which board will win first. What will your final
 #       score be if you choose that board?
 
-def giveInput():
-    with open('puzzleinput04.txt', 'r') as file:
-        l1 = file.readline().rstrip().split(',')
-        file.readline() # remove extra line
-        ls = file.readlines()
-
-        i = 0
-        boards = list()
-        while i < len(ls):
-            board = list()
-            while i % 6 != 5:
-                board.append(ls[i].split())
-                i+=1
-            boards.append(board)
-            i+=1
-
-        return l1, boards
-
-def markBoard(board, n):
-    i,j = 0,0
-    while i < len(board):
-        while j < len(board[0]):
-            if board[i][j] == n:
-                board[i][j] = 'x'
-            j+=1
-        i+=1
-        j=0
-
-
-def hasBingo(board):
-    # check horizontal
-    i,j = 0,0
-    while i < len(board) and j < len(board[0]):
-        if board[i][j] != 'x':
-            i+=1
-            j=0
-        else:
-            j+=1
-            if j == len(board[0]):
-                return True
-
-    # check vertical
-    i,j = 0,0
-    while i < len(board) and j < len(board[0]):
-        if board[i][j] != 'x':
-            j+=1
-            i=0
-        else:
-            i+=1
-            if i == len(board):
-                return True
-
-    return False
+from bingo import giveInput,markBoard,hasBingo
 
 def bingoOperator(nums, boards):
     for n in nums:
