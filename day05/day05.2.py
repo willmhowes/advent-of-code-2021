4660e5a795d6e4aead70aea8fa90cb8a724e0c8e
Will Howes <willmhowes@gmail.com>
2021-12-06 12:01:20 -0800

    improve solution using sets

--- day05/day05.2.py
+++ day05/day05.2.py
@@ -28,10 +28,10 @@ def giveInput():
             l = file.readline().rstrip()
         return ls
 
-def fillGrid(ls):
-    grid = [[0 for x in range(MAX_LENGTH)] for x in range(MAX_LENGTH)]
+def countOverlaps(ls):
+    markedOnce, markedMult = set(), set()
     for l in ls:
-        # compute direction
+        # compute line direction
         left0, left1, right0, right1, d = l
         dx,dy = 0,0
         if right0 > left0: dy = 1
@@ -39,43 +39,36 @@ def fillGrid(ls):
         if right1 > left1: dx = 1
         else:              dx = -1
 
+        c, r = left0, left1 # index for tracking position of line
+
         # diagonal line
         if d == 2:
-            c, r = left0, left1
             while c != right0+dy:
-                grid[r][c] += 1
-                c+=dy
-                r+=dx
+                if (r,c) in markedOnce: markedMult.add((r,c))
+                else:                   markedOnce.add((r,c))
+                c+=dy # increment column index
+                r+=dx # increment row index
 
         # vertical line
         elif d == 1:
-            c = left0
             while c != right0+dy:
-                grid[left1][c] += 1
-                c+=dy
+                if (r,c) in markedOnce: markedMult.add((r,c))
+                else:                   markedOnce.add((r,c))
+                c+=dy # increment column index
 
         # horizontal line
         else:
-            r = left1
             while r != right1+dx:
-                grid[r][left0] += 1
-                r+=dx
+                if (r,c) in markedOnce: markedMult.add((r,c))
+                else:                   markedOnce.add((r,c))
+                r+=dx # increment row index
 
-    return grid
-
-def countGridRepeats(grid):
-    ct = 0
-    for r in grid:
-        for c in r:
-            if c > 1:
-                ct+=1
-    return ct
+    return len(markedMult)
 
 def main():
     ls = giveInput()
-    grid = fillGrid(ls)
-    count = countGridRepeats(grid)
-    print(count)
+    ct = countOverlaps(ls)
+    print(ct)
 
 if __name__ == "__main__":
     main()
